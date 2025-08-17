9397893779f3d0d012d155b7cd2b6cff25327447
Miasma48 <42460157+Miasma48@users.noreply.github.com>
2025-08-17 17:31:17 -0400

    updated LC 121 Best time to buy and sell stock

--- 121_best_time_to_buy_and_sell_stock.py
+++ 121_best_time_to_buy_and_sell_stock.py
@@ -21,26 +21,46 @@ Note that buying on day 2 and selling on day 1 is not allowed because you must b
 Input: prices = [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transactions are done and the max profit = 0.
+
+Notes:
+We can only choose 1 day to buy and 1 day to sell.
+Thus, we need to track the lowest cost to the left of our position. 
+Thus, we need to track the highest cost to the right of our position. 
+
+The max profit the smallest we've tracked so far, along with a tracker of curr-smallest
+Thus, the tagged lowest price may not move as we iterate and get current prices. 
+This satifies the temporal property of buying and selling. 
 """
 
 
-# input: list of interger prices
-# process: find largest distance between two index values with a < b
-# output: return distance, or zero 
-# technique/algorithm
+    
 class Solution:
-    def maxProfit(self, prices):
-        # 2 pointer solution
-        # O(n) running time solution
-        # O(n) space
-        lmin = max(prices)
-        mProfit = 0
-        for i in prices:
-            # compare profit
-            mProfit = max(mProfit, i - lmin)
-            # update lmin
-            lmin = min(lmin, i)
-        return mProfit
+
+    def maxProfit(self, prices: List[int]) -> int:
+        '''
+        Brute Force: Two for loops to find the highest day greater than current price. 
+        runtime: O(n**2)
+        space: O(1)
+        '''
+        profit = 0
+        for i in range(len(prices)):
+            for j in range(i+1, len(prices)):
+                profit = max(profit, prices[j] - prices[i])
+        return profit
+
+    def maxProfit(self, prices: List[int]) -> int:
+        '''
+        One pass solution:
+        Track the lowest price seen so far and the max profit that can be made with that price
+        Runtime: O(n)
+        Space = O(1)
+        '''
+        low = prices[0]
+        profit = 0
+        for price in prices:
+            profit = max(profit, price - low)
+            low = min(low, price)
+        return profit
         
 prices = [7,1,5,3,6,4]
 a = Solution()

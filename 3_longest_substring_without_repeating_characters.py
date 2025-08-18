88dd92ce2781fecfb647ef680eb76561ff70b992
Miasma48 <42460157+Miasma48@users.noreply.github.com>
2025-08-18 11:56:10 -0400

    updated LC 3, 424

--- 3_longest_substring_without_repeating_characters.py
+++ 3_longest_substring_without_repeating_characters.py
@@ -16,36 +16,49 @@ Constraints:
     0 <= s.length <= 5 * 104
     s consists of English letters, digits, symbols and spaces.
 """
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # will be using pointers and dictionary
-        # the used characters: last used index
-        used = {}
-        # set left bound
-        left = 0
-        width = 0
-        
-        # iterate the right pointer
-        for right in range(len(s)):
-            # if the char at the right bound hasn't been used yet
-            if s[right] not in used:
-                # we will set width to the max of either the current window, 
-                # or this new window between left and right bounds
-                width = max(width, right - left + 1) 
-            # else if the char at right bound is already used
-            else:
-                # if the char has been used 0 times
-                if used[s[right]] < left:
-                    # set the width to max of either current or new window
-                    width = max(width, right - left + 1)
-                # else the char has been used >= 1 times
+        '''
+        Brute Force Approach: Treat each index as a starting point
+        runtime O(N**2)
+        Space : O(N) to track the characters seen
+        '''
+        longest = 0
+        for i in range(len(s)):
+            seen = set()
+            seen.add(s[i])
+            length = 1
+            for j in range(i+1, len(s)):
+                if s[j] in seen:
+                    longest = max(longest, length)
+                    break
                 else:
-                    # move left bound to current right bound
-                    left = used[s[right]] + 1
-            # set new dictionary entry
-            used[s[right]] = right
-        return width
+                    seen.add(s[j])
+                    length += 1
+        return longest
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        '''
+        Sliding Window: 
+        A. We track each last seen index per character. 
+        B. If there is a repeating character, to make the window valid, we move left = lastPos + 1
+        '''
+        pos = {}
+        res = 0
+        left = 0
+        for right in range(len(s)):
+            # repeat
+            if s[right] in pos:
+                # The repeated character may be outside our current window
+                # Thus, we want the max, which is guaranteed to be in window
+                left = max(left, pos[s[right]] + 1)
+            pos[s[right]] = right
+            res = max(res, right - left + 1)
+        return res
+        
+        
+                
+        
         
 alpha = Solution()
 s = 'pwwkew'
--- 424_longest_repeating_character_replacement.py
+++ 424_longest_repeating_character_replacement.py
@@ -1,28 +1,109 @@
+'''
+424. Longest Repeating Character Replacement
+Medium
+You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+ 
+
+Example 1:
+
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+Example 2:
+
+Input: s = "AABABBA", k = 1
+Output: 4
+Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4.
+There may exists other ways to achieve this answer too.
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only uppercase English letters.
+0 <= k <= s.length
+'''
+
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-    	"""
-    	Using a 2 pointer approach to a sliding window method
-    	Left pointer starts at index 0
-    	The sliding window will expand to the right according to allowed changes
-    	The sliding window will shift right according to allowed changes
-		"""
-        count = {}
+        ''' 
+        Brute Force: 
+        1. Treat each index as the starting index of all its possible substrings
+        2. We track each occurence and the most frequently occuring character
+        3. The condition is the window - freqMost <= k. Otherwise break. 
+        Runtime: O(n**2)
+        Space: O(26) = O(1)
+        '''
         res = 0
-        
-        l = 0
-        maxf = 0
-        # the right pointer iterates through after left pointer
-        for r in range(len(s)):
-        	# each time right pointer shifts, add 1 to the count of that letter in dictionary
-            count[s[r]] = 1 + count.get(s[r], 0)
-            # set maximum freq variable. Either stays the same, or the new updated letter from right shift
-            maxf = max(maxf, count[s[r]])
-            # if the length of the window - maxfreq is greater than allowed changes
-            if (r - l + 1) - maxf > k:
-            	# remove count from left pointer
-                count[s[l]] -= 1
-                # shift the left pointer
-                l += 1
-            # set res to max(current value, or window left)
-            res = max(res, r - l + 1)
+        for i in range(len(s)):
+            tracker = collections.defaultdict(int) # {char:count}
+            for j in range(i, len(s)):
+                tracker[s[j]] += 1
+                maxChar, maxFreq = max(tracker.items(), key = lambda x: x[1])
+                if (j-i+1) - maxFreq > k:
+                    break
+                res = max(res, (j-i+1))
         return res
+
+    def characterReplacement(self, s: str, k: int) -> int:
+        '''
+        Sliding window: 
+        (1) Expand window on condition that window - maxFreq <= k
+        (2) If condition holds, update result
+        (3) Else condition false, move left ptr++ until condition is valid
+        
+        Runtime: O(n)
+        Space: O(n)
+        '''
+        res = 0
+        left = 0
+        tracker = collections.defaultdict(int)
+        for right in range(len(s)):
+            tracker[s[right]] += 1
+            maxChar, maxFreq = max(tracker.items(), key = lambda x: x[1])
+            while (right - left + 1) - maxFreq > k:
+                tracker[s[left]] -= 1
+                left += 1
+            res = max(res, (right - left + 1))
+        return res
+
+    def characterReplacement(self, s: str, k: int) -> int:
+        '''
+        Sliding window without maxFreq O(26) 
+        Runtime: O(n)
+        Space: O(n)
+        '''
+        res = 0
+        left = 0
+        tracker = collections.defaultdict(int)
+        maxFreq = 0
+        for right in range(len(s)):
+            tracker[s[right]] += 1
+            maxFreq = max(maxFreq, tracker[s[right]])
+            while (right - left + 1) - maxFreq > k:
+                tracker[s[left]] -= 1
+                left += 1
+            res = max(res, (right-left+1))
+        return res 
+
+    
+
+                    
+            
+
+
+
+
+    
+            
+
+                
+                
+                
+                
+                
+        
+        

ab6b3982c1a97ac164c8e52ee1f3db43c68b1f19
Jon Dinh <87919907+jhd9252@users.noreply.github.com>
2025-08-19 23:40:17 -0400

    updated 2 add two numbers

--- 2_add_two_numbers.py
+++ 2_add_two_numbers.py
@@ -49,52 +49,60 @@ class Solution:
             curr = curr.next
         return dummy.next
 
+    # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        """
-        Two Pointer Solution with CarryOver
-        """
-        if not l1 and not l2: return ListNode()
-        elif not l1: return l2
-        elif not l2: return l1
-    
-        p1, p2 = l1, l2
-        dummy = ListNode()
-        head = dummy
-        carry = False
-        while p1 and p2:
-            val = p1.val + p2.val if not carry else p1.val + p2.val + 1
-            if val >= 10:
-                val %= 10
-                carry = True
-            else:
-                carry = False
-            dummy.next = ListNode(val)
-            dummy = dummy.next
-            p1, p2 = p1.next, p2.next
-        
-        if not p1:
-            while p2:
-                val = p2.val if not carry else p2.val + 1
-                if val >= 10:
-                    val %= 10
-                    carry = True
-                else:
-                    carry = False
-                dummy.next = ListNode(val)
-                dummy = dummy.next
-                p2 = p2.next
-        if not p2:
-            while p1:
-                val = p1.val if not carry else p1.val + 1
-                if val >= 10:
-                    carry = True
-                    val %= 10
-                else:
-                    carry = False
-                dummy.next = ListNode(val)
-                dummy = dummy.next
-                p1 = p1.next
-        if carry:
-            dummy.next = ListNode(1)
-            dummy = dummy.next
-        return head.next
+        ''' Brute Force Naive: Get the ints, reverse, add, create new linked list '''
+        pass
+
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        '''
+        Two Pointers:
+        '''
+        dummyHead = ListNode(0)
+        tail = dummyHead
+        carry = 0
+
+        while l1 is not None or l2 is not None or carry != 0:
+            digit1 = l1.val if l1 is not None else 0
+            digit2 = l2.val if l2 is not None else 0
+
+            sum = digit1 + digit2 + carry
+            digit = sum % 10
+            carry = sum // 10
+
+            newNode = ListNode(digit)
+            tail.next = newNode
+            tail = tail.next
+
+            l1 = l1.next if l1 is not None else None
+            l2 = l2.next if l2 is not None else None
+
+        result = dummyHead.next
+        dummyHead.next = None
+        return result
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        ''' 
+        Less verbose 
+        RT: O(n+m)
+        Space: O(1)
+        '''
+        dummy = cur = ListNode(0)           # create dummy node to return, curr to move
+        carry = 0                           # this is the carry for addition
+        while l1 or l2 or carry:            # while there is still something to add
+            if l1:                          # if l1 exists, add its value to carry 
+                carry += l1.val
+                l1 = l1.next
+            if l2:                          # if l2 exists, add its value to carry   
+                carry += l2.val
+                l2 = l2.next
+            cur.next = ListNode(carry%10)   # creates a node with leftover after mod 10
+            cur = cur.next
+            carry //= 10                    # set carry to be 0 or 1 depending on div 10
+        return dummy.next
+                
+            

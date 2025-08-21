cdb278d0b39628f3a702bdc2db2bd817d538ffda
Jon Dinh <87919907+jhd9252@users.noreply.github.com>
2025-08-21 15:58:39 -0400

    added 105 construct binary tree from preorder and inorder arrays

---
+++ 105-construct-binarytree.py
@@ -0,0 +1,44 @@
+'''
+105. Construct Binary Tree from Preorder and Inorder Traversal
+Solved
+Medium
+
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary
+tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    '''
+    preorder is [root, left, right]
+    in order is [left, root, right]
+    We see that preorder gives us the current parent of a local triangle.
+    The index that that current parent in inorder array gives us the left and right subtrees.
+    Thus, we create a tree from the top down.
+    Take the first entry in pre order, create it as a parent node.
+    Then set its left and right children / subtrees to be inorder split at its index. 
+    It will recurse down until possible children is nothing left, therefore creating NULLs.
+    return the current triangle parent upwards as children, until root is returned fully.
+    '''
+
+
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        # for each num in preorder
+        # find index in inorder
+        # set left child and right child to be arr[:idx] and arr[idx+1:]
+        # recurse down, return up the current created node
+        root = None
+        if inorder:
+            root = preorder.pop(0)      # this is current root
+            index = inorder.index(root) # get index in inorder [left:right]
+            root = TreeNode(root)
+            root.left = self.buildTree(preorder, inorder[:index])
+            root.right = self.buildTree(preorder, inorder[index+1:])
+        return root
+

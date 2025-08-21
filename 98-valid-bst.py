d50d673307fcec544f06488c389e4e95ad2b18d8
Jon Dinh <87919907+jhd9252@users.noreply.github.com>
2025-08-21 00:51:03 -0400

    updated 98 valid bst

--- 98-valid-bst.py
+++ 98-valid-bst.py
@@ -10,6 +10,25 @@ A valid BST is defined as follows:
 The left subtree of a node contains only nodes with keys strictly less than the node's key.
 The right subtree of a node contains only nodes with keys strictly greater than the node's key.
 Both the left and right subtrees must also be binary search trees.
+
+ Binary Search Tree if 
+    left <= node <= right
+    left and right subtrees are also BST
+    The left subtree must contain only keys < node
+    the right subtree must contain only keys > node.
+    This is easy if viewing from the current level : left.val < node.val < right.val
+    But expanding our view to the max(left) < node.val < min(right) is the challenge. 
+
+Note: 
+(1) It might feel intuitive to say that if left < node < right, then it is a BST, but this is not true.
+(2) Looking at the local node and its children is not enough to determine if the entire tree is a valid BST. 
+(3) Comparing a child to its parent is natural, but don't forget the entire ancestry of the node.
+(4) The progenitor is the root, and is allowed to be any value, so we can set the initial bounds to -inf and inf.
+(5) As the range is passed down, it gets more narrow.
+(6) Direct comparisons are necessary but not sufficient to determine if the entire tree is a valid BST.
+(7) Abiding by the ancestry range implicitly checks the node to its parent and grandparent, etc.
+
+
 '''
 
 # Definition for a binary tree node.
@@ -19,16 +38,7 @@ Both the left and right subtrees must also be binary search trees.
 #         self.left = left
 #         self.right = right
 class Solution:
-    '''
-    Binary Search Tree if 
-    left <= node <= right
-    left and right subtrees are also BST
-    The left subtree must contain only keys < node
-    the right subtree must contain only keys > node.
-    This is easy if viewing from the current level : left.val < node.val < right.val
-    But expanding our view to the max(left) < node.val < min(right) is the challenge. 
 
-    '''
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         '''
         DFS with nonlocal min and max values

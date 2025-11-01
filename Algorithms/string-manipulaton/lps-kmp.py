0f5eb80606ec31a00ec08974fff60a559beb9b68
Jon Dinh <87919907+jhd9252@users.noreply.github.com>
2025-10-31 21:58:39 -0400

    reviewed string substring matching with LPS-KPM algorithm

--- Algorithms/string-manipulaton/lps-kmp.py
+++ Algorithms/string-manipulaton/lps-kmp.py
@@ -1,44 +1,39 @@
 
 # easy way
 
-pip install kmp-algorithm
+!pip instal kmp-algorithm
 
-from kpm install KPMMatcher
-matcher = KPMMatcher(subtring)
-index = matcher.search(string) # start index or -1 
+from kmp import KPMMatcher
+matcher = KMPMatcher(substring)
+index = matcher.search(string) # start index or -1
 
 
 # LPS - longest prefix suffix
-
 def lps(substring):
+    # compare substring indexes with last match within it
     m = len(substring)
     lps = [0] * m
     left = 0
     for right in range(1, m):
-        while substring[left] != substring[right] and j > 0:
+        while substring[left] != substring[right] and left > 0:
             left = lps[left - 1]
-
         if substring[left] == substring[right]:
             left += 1
-            lps[right] = left 
+            lps[right] = left
 
-    return lps 
+    return lps
 
 def kpm(string, substring):
     m, n = len(string), len(substring)
     lps = lps(substring)
     res = []
     left = 0
-
     for right in range(n):
-
         while string[right] != substring[left] and left > 0:
             left = lps[left - 1]
-
         if substring[left] == string[right]:
             left += 1
             if left == n:
                 res.append(right)
-    
     return res if res else -1
 

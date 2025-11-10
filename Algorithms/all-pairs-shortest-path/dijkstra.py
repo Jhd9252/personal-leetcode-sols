5dadf0e4f8abf6138629e62ad376f7c7f337b917
Jon Dinh <87919907+jhd9252@users.noreply.github.com>
2025-11-10 15:02:00 -0500

    reviewed dijkstra and kruskal union find

--- Algorithms/all-pairs-shortest-path/dijkstra.py
+++ Algorithms/all-pairs-shortest-path/dijkstra.py
@@ -11,18 +11,18 @@ def dijkstra(graph: dict, root) -> dict:
     dist = {node: (float('inf'), None) for node in graph}
     dist[root] = (0, None)
 
-    minPrio = [(0, root)] # heapq minheap -> (dist to node, node)
+    minPrio = [(0, root)]
     while minPrio:
         curr_dist, curr_node = heapq.heappop(minPrio)
         if curr_dist > dist[curr_node]:
             continue
-        for weight, neighbor in graph[curr_node]:
-            if curr_dist + weight < dist[neighbor]:
-                dist[neighbor] = curr_dist + weight
-                heapq.heappush(minPrio, (curr_dist + weight, neighbor))
+        for w, n in graph[curr_node]:
+            if curr_dist + w < dist[n]:
+                dist[n] = curr_dist + w
+                heapq.heappush(minPrio, (curr_dist + w, n))
     return dist
 
-def apsp(graph: dict) -> dict:
+def apsp(graph:dict) -> dict:
     apsp = {}
     for node in graph:
         apsp[node] = dijkstra(graph, node)

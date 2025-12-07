d6a70578fefd0e6e752a8a6db39f7729f318d8cb
Jon Dinh <87919907+jhd9252@users.noreply.github.com>
2025-12-07 11:40:14 -0500

    reviewed cycle detection for graphs with negative weights using bellman fords algorithm

--- Algorithms/cycle-detection/negative-weights.py
+++ Algorithms/cycle-detection/negative-weights.py
@@ -2,60 +2,57 @@
 # relax all edges |V| - 1 times
 # if any edge can be relaxed again
 # then there exists a negative cycle
-
 '''
-Bellman Fords: Single Source shortest path
 
-Intuition: 
-- The shortest path from source to any node is at most |V| - 1 edges.
+Using Bellman Fords SSSP Relaxation |V| - 1 times
+Intution: 
+- The shortest path from source to any node is |V| - 1 edges. 
 - Any path longer than that is not a shortest path
-- Thus, if we relax every single edge |V| -1 times, we can account for all possible shortest paths
-- After, if there is an edge that CAN be relaxed again, there exists a negative cycle. 
+- If we relax every edge |V| - 1 times, we account for all possible shortest paths. 
+- After, if there is an edge that can be relaxed again, there is a negative cycle. 
 
-Input: DAG with non-negative or negative weights 
-Output: Distance Hashmap
+Input: DAG with non-negative weights (assuming) or negative weights (cycle)
+Output: Distance Array 2D
 
 Runtime: O(VE)
-
-Space: O(V + E)
+Space: O(V+E)
 '''
 
-def bellman_ford(graph: dict, root) -> dict:
+def bellmans(graph: dict, root) -> dict:
+    # if required for edge case, get all nodes first, then create
     dist = {node: (float('-inf'), None) for node in graph}
     dist[root] = (0, None)
 
     n = len(graph)
 
-    # for |V| - 1 times 
+    # for all edges
     for _ in range(n):
-        
-        # iterate through all nodes
+        # for all nodes
         for node in graph:
-            # iterate through all edges
+            # for all edges
             for weight, neighbor in graph[node]:
-                
-                # get the dist to this node + distance to neighbor
+                # get dist to current node + dist to next node
                 possible = dist[node][0] + weight
-                
-                # if dist_to_this_node + dist_neighbor < recorded_dist_neighbor
-                # relax
+                # compare distance of next node to recorded
                 if possible < dist[neighbor][0]:
                     dist[neighbor] = (possible, node)
 
+    # should have SSSP unless negative weights
     for node in graph:
         for weight, neighbor in graph[node]:
             if dist[node][0] + weight < dist[neighbor][0]:
                 raise ValueError('negative cycle')
-            
-    return dist
+    return dist 
+
+
 
 def rebuild(dist: dict, target) -> list:
-
     path = []
     while target is not None:
         path.append(target)
         target = dist[target][1]
     return path[::-1]
+
 
 
 

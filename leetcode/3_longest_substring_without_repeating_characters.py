1832b0f69117b5b8e47ac75d793f4834eed216ab
Jon Dinh <87919907+jhd9252@users.noreply.github.com>
2025-12-20 15:04:12 -0500

    Review LC3 Longest Substring without repeating characters

--- leetcode/3_longest_substring_without_repeating_characters.py
+++ leetcode/3_longest_substring_without_repeating_characters.py
@@ -1,28 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr  8 19:25:19 2022
+Problem: Longest Substring without Repeating Characters
 
-@author: jhd9252
-
-LeetCode Problem: 3. Longest Substring Without Repeating Characters
-Test Cases: 987 / 987 test cases passed.
-Runtime: 67 ms (runtime beats 85.06 % of python3 submissions)
-Memory Usage: 14.2 MB (memory usage beats 16.14 % of python3 submissions.)
-
-
-Given a string s, find the length of the longest substring without repeating characters.
+Given a string 's', find the length of the longet substring without repeating characters
 
 Constraints:
     0 <= s.length <= 5 * 104
     s consists of English letters, digits, symbols and spaces.
+
+Say we have string:
+'abcdefgahi'
+start index = 0
+repeat index = 7
+Then the length of current string without repeats is end - start
+at that point, we want to keep the longest substring of substring without repeats
+Keep track of each last seen index in 26 bucket map
 """
 class Solution:
+    # Runtime: O(n^2)
+    # Space: O(26)
     def lengthOfLongestSubstring(self, s: str) -> int:
-        '''
-        Brute Force Approach: Treat each index as a starting point
-        runtime O(N**2)
-        Space : O(N) to track the characters seen
-        '''
         longest = 0
         for i in range(len(s)):
             seen = set()
@@ -37,33 +34,31 @@ class Solution:
                     length += 1
         return longest
 
+    # sliding window
+    # track a map {char: last seen index}
+    # if there is a repeating character, move the start position
     def lengthOfLongestSubstring(self, s: str) -> int:
-        '''
-        Sliding Window: 
-        A. We track each last seen index per character. 
-        B. If there is a repeating character, to make the window valid, we move left = lastPos + 1
-        '''
-        pos = {}
+        position = {}
         res = 0
-        left = 0
-        for right in range(len(s)):
-            # repeat
-            if s[right] in pos:
-                # The repeated character may be outside our current window
-                # Thus, we want the max, which is guaranteed to be in window
-                # if outside window, dont count it 
-                # if inside our window, we move left so the window is valid
-                left = max(left, pos[s[right]] + 1)
-            # no matter what, add in right
-            pos[s[right]] = right
-            # now that window is valid, check res
-            res = max(res, right - left + 1)
-        return res
-        
-        
-                
-        
-        
-alpha = Solution()
-s = 'pwwkew'
-print(alpha.lengthOfLongestSubstring(s))
+        start = 0
+
+        for end in range(len(s)):
+            # check if char has been seen before
+            if s[end] in position:
+                # check if last known position is within current window
+                # if it is, adjust to index + 1 else, outside position and less than start
+                # therefore, take the larger index
+                start = max(start, position[s[end]]+1)
+            # no matter what, end position is now repeat free
+            # record last known
+            position[s[end]] = end
+            # window is valid, current char is recorded
+            # check the longest result
+            res = max(res, end - start + 1)
+        return res 
+
+if __name__ == '__main__':
+    alpha = Solution()
+    s = 'pwwkew'
+    print(alpha.lengthOfLongestSubstring(s))
+    # wke, kew, 3

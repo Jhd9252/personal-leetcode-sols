e2024623c36953f0ee132634269cd4cc41fd21ad
Jon Dinh <87919907+jhd9252@users.noreply.github.com>
2026-01-10 20:47:28 -0500

    reviewed longest palindromic substring

--- leetcode/5_longest_palindromic_string.py
+++ leetcode/5_longest_palindromic_string.py
@@ -5,12 +5,13 @@ class Solution:
     M2. DP O(n**2) runtime, O(n**2) sapce
     '''
     def m1_check(self, s, l, r) -> bool:
+        ''' Helper for Brute Force - pick all possible start and ends '''
         while l < r:
             if s[l] != s[r]:
                 return False
             l += 1
             r -= 1
-        return True
+        return True 
             
     def longestPalindrome(self, s: str) -> str:
         ''' 
@@ -23,7 +24,9 @@ class Solution:
             for j in range(i, len(s)):
                 if self.m1_check(s, i, j):
                     res = max(res, s[i:j+1], key = len)
-        return res 
+        return res
+    
+    
     
     def checkPalindrome(self, s, l, r):
         res = ''
@@ -42,10 +45,10 @@ class Solution:
         '''
         res = ''
         for i in range(len(s)):
-            even = self.checkPalindrome(s, i, i+1)
-            odd = self.checkPalindrome(s, i, i)
-            res = max([res, even, odd], key = lambda x: len(x))
-        return res
+            even = self.checkPalindrome(s, i, i+1) # even length
+            odd = self.checkPalindrome(s, i, i) # odd length
+            res = max([res, even, odd], key = len)
+        return res 
 
     def longestPalindrome(self, s: str) -> str:
         ''' 
@@ -53,30 +56,31 @@ class Solution:
         runtime: O(n**2)
         space: O(n**2)
         '''
-        
-        # NOTE: We know that dp[i][j] = s[i] == s[j] and (dp[i+1][j-1] or j == i + 1)
-        # NOTE: at each spot, there are two possible ways for palindrome. single source, two source
-        # NOTE: dp[i][j] = s[i] == [sj] checks if current bounds are equal
-        # NOTE: If they are, then it depends on inner bounds dp[i+1][j-1] -> covers single source convergence
-        # NOTE: But double source is next to each other, so if the two j=i+1 -> covers double sourcem since i cant cross j
+        # Given a single instance of (start, end) = (i, j)
+        # We know that it depends on if the previous inner string is palindrome dp[i+1][j-1]
+        # We know strings are single source or double source
+        # We know the edge cases 
+        # so each instance, we need to check if the current bounds are equal dp[i][j] == s[i] == s[j]
+        # If they are, then we check if inner string is palindrome and valid dp[i+1][j-1] -> single source
+        # If they are, then check inner string palindrome and double source dp[i][j=i+1]
+        # NOTE: Then we know current recurrent relation
 
-        # NOTE: So we know that dp[i][j] has a dependecy on subproblem [i+1][j-1]
-        # Then for (i, j), we must prev have 1 row below, and i col left
-        #           [i,j]
-        # [i+1, j-1]
-
+        # set dp table
         dp = [[False] * len(s) for i in range(len(s))]
         res = s[0]
-        for i in range(len(s)):
-            dp[i][i] = True 
 
+        # set flags / every single char is palindrome to itself
+        for i in range(len(s)):
+            dp[i][i] = True
+        
         # ranging over columns (left to right) for (j-1) dependency
         for j in range(len(s)):
-            # range over rows (top down) for (i+1) dependecy in next col
-            # only go up until J, covered double source, no repeating calculations
             for i in range(j):
-                dp[i][j] = (s[i]==s[j]) and (dp[i+1][j-1] or j == i + 1)
-                if j - i + 1 > len(res):
+                dp[i][j] = (s[i] == s[j]) and (dp[i+1][j-1] or j == i + 1)
+                if j - 1 + 1 > len(res):
                     res = s[i:j+1]
         return res
+
+
+     
        
